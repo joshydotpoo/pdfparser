477e4f97624eea81aec301b5119a0bb39e2e151d
joshydotpoo <j.freilichd@gmail.com>
2018-11-03 14:18:18 -0700

    attempt 2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
-$parser = new src\Smalot\PdfParser\Parser();
+include 'src/Smalot/PdfParser/Parser.php';
+$parser = new Parser();
 $pdf    = $parser->parseFile('samples/Document1_foxitreader.pdf');
-
 $text = $pdf->getText();
 echo $text;
 ?>
